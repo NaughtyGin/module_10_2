bea00b9ec0e7dbb05f220b4ef51ff1296abec763
NaughtyGin <as519155@gmail.com>
2024-10-22 10:20:05 +1000

    Add files via upload

--- module_10_2.py
+++ module_10_2.py
@@ -3,14 +3,17 @@ from time import sleep
 
 
 class Knight(Thread):
-    knights = 0
+    knights = []
+
+    def __new__(cls, *args, **kwargs):
+        return object.__new__(cls)
 
     def __init__(self, name, power, enemies=100):
         super().__init__()
         self.name = name
         self.power = power
         self.enemies = enemies
-        Knight.knights += 1
+        Knight.knights.append(self)
 
     def run(self):
         count_days = 0
@@ -19,15 +22,21 @@ class Knight(Thread):
             self.enemies -= self.power
             sleep(1)
             count_days += 1
-            print(f'{self.name} сражается {count_days} дней(дня), осталось {self.enemies} воинов.')
+            if self.enemies <= 0:
+                print(f'{self.name} сражается {count_days} дней(дня), уже не осталось воинов.')
+            else:
+                print(f'{self.name} сражается {count_days} дней(дня), осталось {self.enemies} воинов.')
         else:
             print(f'{self.name} одержал победу спустя {count_days} дней(дня)!')
 
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
-first_knight.start()
-second_knight.start()
-first_knight.join()
-second_knight.join()
+third_knight = Knight('Илья Муромец', 30)
+threads = []
+for obj in Knight.knights:
+    obj.start()
+    threads.append(obj)
+for thread in threads:
+    thread.join()
 print('Все битвы завершены!')
